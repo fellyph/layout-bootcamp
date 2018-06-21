c7cdf1ebc1d4876b6792e74f41c7493f632c2a26
Fellyph Cintra <fellyph.cintra@gmail.com>
2018-06-21 16:05:31 +0100

    instaling html plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,71 +1,82 @@
 const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: {
-        bundle: './src/main.js'
+  entry: {
+    bundle: './src/main.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
     },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader'
-            }
-        },
+    {
+      test: /\.html$/,
+      use: [
         {
-            test: require.resolve('knockout'),
-            use: [{
-                loader: 'expose-loader',
-                options: 'ko'
-            }]
-        },
-        {
-            test: /\.css$/,
-            use: [
-                {
-                    loader: 'style-loader'
-                },
-                {
-                    loader: 'css-loader',
-                    options: {
-                        modules: true,
-                        importLoaders: 1,
-                        localIdentName: '[name]_[local]_[hash:base64]',
-                        sourceMap: true,
-                        minimize: true
-                    }
-                }
-            ]
-        },
-        {
-            test: /\.less$/,
-            use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: ['css-loader', 'less-loader']
-            })
-        },
-        {
-            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]',
-                    outputPath: 'fonts/'
-                }
-            }]
-        }]
+          loader: 'html-loader',
+          options: { minimize: true }
+        }
+      ]
     },
-    plugins: [
-        new ExtractTextPlugin('[name].css'),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        })
-    ],
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
-    }
+    {
+      test: /\.css$/,
+      use: [{
+        loader: 'style-loader'
+      },
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          importLoaders: 1,
+          localIdentName: '[name]_[local]_[hash:base64]',
+          sourceMap: true,
+          minimize: true
+        }
+      }]
+    },
+    {
+      test: /\.less$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'less-loader']
+      })
+    },
+    {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/'
+        }
+      }]
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css'),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+
 }
